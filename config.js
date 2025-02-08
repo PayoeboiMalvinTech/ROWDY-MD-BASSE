efde1560cc7bedef4cf608ea16a3b0b8dc514e54
PayoeboiMalvinTech <malvink002@gmail.com>
2025-02-08 16:57:14 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID ||"13lkXCKQ#kgM3CZej2MGfhpHY0CQ04mpsCAFZf_8k8_yjELoTK-A",
 ALIVE_IMG: process.env.ALIVE_IMG ||"https://i.ibb.co/zZ13BVL/9674.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG ||"𝐇𝐄𝐋𝐋𝐎 𝐢 𝐀𝐌 𝐑𝐎𝐖𝐃𝐘 𝐌𝐃 𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏 𝐁𝐎𝐓. 𝐂𝐑𝐄𝐀𝐓𝐄 𝐁𝐘 𝐃𝐀𝐑𝐊 𝐒𝐈𝐇𝐈𝐍𝐀...🌝💚 ",
 };
