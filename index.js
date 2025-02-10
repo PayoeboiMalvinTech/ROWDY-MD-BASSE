7d7df5c0e17a1ae22ce6dc5ea88f54d9ce8910ff
PayoeboiMalvinTech <malvink002@gmail.com>
2025-02-10 03:28:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const axios = require('axios')
 const { File } = require('megajs')
 const prefix = '.'
 
-const ownerNumber = ['26300000000']
+const ownerNumber = ['263780934873']
 
 //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
